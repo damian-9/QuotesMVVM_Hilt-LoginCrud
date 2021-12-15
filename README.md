d1a8a262690dbdf77116cbd854f2b785e0b65519
damian-9 <damia.marjim98@gmail.com>
2021-12-15 11:35:37 -0600

    Actualizado

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Este proyecto está pensado para fines académicos y se irá agregando nuevas características conforme se avance en el curso
-
---
+++ app/src/main/java/dev/cardoso/quotesmvvm/core/constants.kt
@@ -0,0 +1,8 @@
+package dev.cardoso.quotesmvvm.core
+
+
+
+const val BASE_URL ="http://192.168.8.52:8080"
+const val API_PATH="quotes.php"
+const val LOGIN_URL= "/api/v1/users/login"
+const val QUOTES_URL= "/api/v1/quotes"
